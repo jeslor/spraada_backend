fb72b5d0282ba9a1cae75755fa5e3e23d6030dbd
Jeslor Ssozi <jbestius@gmail.com>
2025-12-05 19:23:16 +0300

    add logic to compress images before sending them to backend

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,14 +1,11 @@
 import {
   Controller,
   Post,
-  Body,
   UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { UploadService } from './upload.service';
-import { CreateUploadDto } from './dto/create-upload.dto';
-import { UpdateUploadDto } from './dto/update-upload.dto';
 
 @Controller('upload')
 export class UploadController {
@@ -16,10 +13,7 @@ export class UploadController {
 
   @Post('images')
   @UseInterceptors(FilesInterceptor('images'))
-  create(
-    @Body() createUploadDto: CreateUploadDto,
-    @UploadedFiles() files: Express.Multer.File[],
-  ) {
+  create(@UploadedFiles() files: Express.Multer.File[]) {
     return this.uploadService.uploadImages(files);
   }
 }
