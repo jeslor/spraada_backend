9eedc3220acc394bda75331dcfc61702b1a88c9c
Jeslor Ssozi <jbestius@gmail.com>
2026-01-25 15:11:55 +0100

    mark it as public end point

--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { HomeService } from './home.service';
 import { get } from 'http';
+import { isPublicEndpoint } from 'src/Auth/decorator/public-endpoint.decorator';
 
 @Controller()
 export class HomeController {
   constructor(private readonly homeService: HomeService) {}
+  @isPublicEndpoint()
   @Get()
   getHome(): {
     message: string;
